39a17eb5cb5e19daca557393419571d5250c8f3e
User <Vislapyz@mail.ru>
2024-09-04 15:15:46 +0300

    Заполнение модели User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name="Email")
+    first_name = models.CharField(max_length=150, verbose_name="Имя", **NULLABLE)
+    last_name = models.CharField(max_length=200, verbose_name="Фамилия", **NULLABLE)
+    country = models.CharField(max_length=150, verbose_name="Страна", **NULLABLE)
+    phone = models.CharField(max_length=35, verbose_name="Телефон", **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"

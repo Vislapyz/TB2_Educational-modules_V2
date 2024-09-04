134bdf1b360e46640044d282c4d90691ed8d5140
User <Vislapyz@mail.ru>
2024-09-04 18:13:53 +0300

    Заполнение модели User и models

--- modules/models.py
+++ modules/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class Module(models.Model):
+    """Модель образовательные модули"""
+
+    number = models.IntegerField(verbose_name="Порядковый номер")
+    name = models.CharField(max_length=100, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание", **NULLABLE)
+
+    def __str__(self):
+        return f"Модуль {self.name}"
+
+    class Meta:
+        verbose_name = "Модуль"
+        verbose_name_plural = "Модули"

5c10ebef9d825d45c26bb91bad270e3ee490a3d0
User <Vislapyz@mail.ru>
2024-09-04 19:27:43 +0300

    Заполнение модели User и models

--- modules/views.py
+++ modules/views.py
@@ -1,1 +1,21 @@
+from rest_framework import generics
 
+
+class ModuleCreateAPIView(generics.CreateAPIView):
+    pass
+
+
+class ModuleListAPIView(generics.ListAPIView):
+    pass
+
+
+class ModuleRetrieveAPIView(generics.RetrieveAPIView):
+    pass
+
+
+class ModuleUpdateAPIView(generics.UpdateAPIView):
+    pass
+
+
+class ModuleDestroyAPIView(generics.DestroyAPIView):
+    pass
